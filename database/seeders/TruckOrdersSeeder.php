93fdc5aeef6df6b58b2a649d349985c14dce696b
ahnge <ahnge226@gmail.com>
2023-12-11 11:00:51 +0630

    truck order seeder to seed some orders associated with trucks

--- database/seeders/TruckOrdersSeeder.php
+++ database/seeders/TruckOrdersSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Truck;
+use App\Models\TruckOrders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,18 @@ class TruckOrdersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $trucks = Truck::all();
+        $orders = Order::all();
+
+        foreach ($trucks as $truck) {
+            // Get a random order from the existing orders
+            $randomOrder = $orders->random();
+
+            // Associate the random order with the truck using the pivot table (TruckOrder)
+            TruckOrders::create([
+                'truck_id' => $truck->id,
+                'order_id' => $randomOrder->id,
+            ]);
+        }
     }
 }
