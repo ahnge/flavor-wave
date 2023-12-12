4ebc8b5d0aa2e27874251b0a2965119ccd688a60
LinnPyaePyae <107622219+LinnPyaePyae@users.noreply.github.com>
2023-12-12 12:06:41 +0630

    web sales update

--- routes/web/sales.php
+++ routes/web/sales.php
@@ -3,3 +3,15 @@
 use App\Http\Controllers\PreorderController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['admin','sales'])->group(function (){
+
+    Route::controller(PreorderController::class)->group(function () {
+        Route::get("preorders", "preorderLists")->name("preorder.preorderList");
+        Route::post("preorders/check-status", "checkStatus")->name("preorder.checkStatus");
+    });
+
+    Route::get('/preorders/{preorder}',[PreorderController::class, 'showOrder'])->name('preorder.edit');
+    Route::post('/preorders/{preorder}',[PreorderController::class, 'changeOrderStatus'])->name('preorder.changeStatus');
+
+
+});
