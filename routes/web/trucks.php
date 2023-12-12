44abcda0f4c4188973202e0383ca20eae50f0f48
ahnge <ahnge226@gmail.com>
2023-12-12 11:45:08 +0630

    change ui and some logic to change order status

--- routes/web/trucks.php
+++ routes/web/trucks.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TruckController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['admin','logistics'])->group(function (){
+Route::middleware(['admin', 'logistics'])->group(function () {
 
 
     // Truck detail page
@@ -11,6 +11,4 @@ Route::middleware(['admin','logistics'])->group(function (){
 
     // Update order status
     Route::put('/trucks/orders/{orderId}/update-status', [TruckController::class, 'updateOrderStatus']);
-
-
 });
