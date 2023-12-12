18481707e99e9d2a2e91504dd5c4ee26fec88b7f
LinnPyaePyae <linnpyae175@gmail.com>
2023-12-12 11:59:29 +0630

    warehouse web update

--- routes/web/warehouse.php
+++ routes/web/warehouse.php
@@ -3,14 +3,8 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("warehouse")->controller(ProductController::class)->group(function () {
-    Route::get("product-list", "productList")->name("warehouse.productList");
-    Route::put("product-edit/{id}", "edit")->name("warehouse.productEdit");
 
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['admin','warehouse'])->group(function (){
-
-
-
+Route::middleware(['admin', 'warehouse'])->prefix("warehouse")->group(function () {
+    Route::get("product-list", [ProductController::class, 'productList'])->name("warehouse.productList");
+    Route::put("product-edit/{id}", [ProductController::class, 'edit'])->name("warehouse.productEdit");
 });
