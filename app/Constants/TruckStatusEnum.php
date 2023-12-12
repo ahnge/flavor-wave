09a2976e6298827307f99fa09f6fe5fbc2c4eacc
ahnge <ahnge226@gmail.com>
2023-12-12 11:54:25 +0630

    fix truck enum bug

--- app/Constants/TruckStatusEnum.php
+++ app/Constants/TruckStatusEnum.php
@@ -2,26 +2,13 @@
 
 namespace App\Constants;
 
-enum OrderStatusEnum: int
+enum TruckStatusEnum: int
 {
     case AtWarehouse = 0;
     case OnTheRoad = 1;
 
 
-    public static function getLabelForAdmins(int $value): string
-    {
-        switch ($value) {
-            case self::AtWarehouse->value:
-                return 'At warehouse';
-            case self::OnTheRoad->value:
-                return 'On the road';
-            default:
-                return 'Unknown';
-        }
-    }
-
-
-    public static function getLabelForDistributors(int $value): string
+    public static function getLabel(int $value): string
     {
         switch ($value) {
             case self::AtWarehouse->value:
