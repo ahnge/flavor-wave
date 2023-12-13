a34594ef128b293b4c51321f5b623ade047f1eb4
HeinZarNe <heinzarne2522005@gmail.com>
2023-12-13 11:54:05 +0630

    Logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,9 @@
 <nav class="bg-neutral-100 border-gray-200 dark:bg-gray-800">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="./index.html" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <a href="{{ route('distributor.index') }}" class="flex items-center space-x-3 rtl:space-x-reverse ps-10">
+      <img src="{{ asset('assets/images/sig.png') }}" class="w-8  " alt="Flowbite Logo">
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flavor Wave</span>
-    </a>
+  </a>
 
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
 
