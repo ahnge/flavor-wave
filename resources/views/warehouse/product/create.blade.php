e8cac7fd05e4af28e13385110c997f11a3d35a73
ahnge <ahnge226@gmail.com>
2023-12-14 00:40:39 +0630

    product image place holder before uploading to server

--- resources/views/warehouse/product/create.blade.php
+++ resources/views/warehouse/product/create.blade.php
@@ -7,7 +7,7 @@
       <button type="button"
         class="py-2.5   px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
     </a>
-    <div class=" mx-auto   rounded-md mt-7">
+    <div class=" mx-auto rounded-md mt-7">
 
 
 
@@ -31,12 +31,18 @@
             required>
         </div>
 
-        <div class="mb-4">
-          <label for="product_photo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
-            Photo</label>
-          <input type="file" id="product_photo" name="product_photo"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required>
+        <div class="flex justify-start items-center gap-8">
+          <div class="mb-4">
+            <label for="product_photo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
+              Photo</label>
+            <input type="file" id="product_photo" name="product_photo" accept="image/*"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required>
+          </div>
+
+          <!-- Hidden product photo container -->
+          <div id="product-photo-placeholder" class="hidden"></div>
+
         </div>
 
         <div class="mb-4">
@@ -77,4 +83,22 @@
       </form>
     </div>
   </section>
+@endsection
+
+@section('scripts')
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      const productPhoto = document.querySelector('#product_photo');
+      const placeholder = document.querySelector('#product-photo-placeholder');
+
+      productPhoto.addEventListener("change", () => {
+        let imgSrc = URL.createObjectURL(productPhoto.files[0]);
+        placeholder.innerHTML += `
+        <img src="${imgSrc}" class="rounded-box w-auto max-h-[5rem] object-contain" />
+      `;
+        placeholder.classList.remove('hidden');
+      })
+
+    });
+  </script>
 @endsection
