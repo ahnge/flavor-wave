8f06eea026bdf526a7638ff43c69c41555ba5753
HeinZarNe <heinzarne2522005@gmail.com>
2023-12-13 12:38:30 +0630

    Back Button in preorder detail

--- resources/views/sales/preorder/index.blade.php
+++ resources/views/sales/preorder/index.blade.php
@@ -43,9 +43,10 @@
                   </g>
               </svg>
           </button> --}}
-
-        <button type="button"
-            class="py-2.5 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
+        <a href="{{ route('preorder.preorderList') }}">
+            <button type="button"
+                class="py-2.5 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
+        </a>
 
         <p class="text-xl my-6 text-gray-900 dark:text-white font-semibold">
             {{ $preorder->order_no }} Details
