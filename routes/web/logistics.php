e508886177e527bb2fa704274e2d98cc0f89254b
Kaung Myat <kaungmyatkyaw255@example.com>
2023-12-14 10:01:19 +0630

    Complete sales and products charts back and raw front but not the dropdown logic

--- routes/web/logistics.php
+++ routes/web/logistics.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LogisticController;
+use App\Http\Controllers\PreorderController;
+use App\Models\Order;
 
 Route::middleware(['admin', 'logistics'])->group(function () {
 
     Route::get('/logistics', [LogisticController::class, "index"])->name("logistic.index");
     Route::get('/trucks/{id}/orders', [LogisticController::class, "orderAssign"])->name('logistic.orderAssign');
     Route::post('/trucks/{id}/orders', [LogisticController::class, "addOrderToTruck"])->name("logistic.addOrderToTruck");
+    Route::get("/product/order-list/{id}", [PreorderController::class, "preorderDetails"])->name("logistic.preOrderDetails");
 });
