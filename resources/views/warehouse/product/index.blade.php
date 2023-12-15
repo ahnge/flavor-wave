9e637e706e4e87e897e513a8b391f7062bd06778
Winthiha <winstonwinn2000@gmail.com>
2023-12-15 10:42:30 +0630

    warehouseAddedAvailableQty, loadingIconAddedToForms, removedReserve

--- resources/views/warehouse/product/index.blade.php
+++ resources/views/warehouse/product/index.blade.php
@@ -21,7 +21,6 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="" selected>Select Operation</option>
                     <option value="expire">Expire</option>
-                    <option value="return">Return</option>
                     <option value="produced">Produced</option>
                 </select>
             </div>
